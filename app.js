1b711765d658da3c8cb60065aec21b11144ccb05
Nick <ryabov.nicholas@gmail.com>
2023-11-01 23:08:10 +0500

    'Файлы_inlex.html_app.js'

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('HELLO, WORLD!');
+const chas = 5;
+const oplata = 80;
+let работа  = 40;
+const dney = 9;
+let a = работа/chas;
+//
+const ch = работа * oplata;
+console.log(a < dney);
+console.log(ch);
